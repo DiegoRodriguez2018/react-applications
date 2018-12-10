82e0a37f202341e5e24888ec44a38d91e7dcc89b
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-12-10 12:10:36 +1100

    create-react-app exercises

---
+++ create-react-app-exercises/portfolio/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1> My portfolio </h1>
+            <p> hi! </p>
+        </div>    
+    );
+}
+
+export default Header;
