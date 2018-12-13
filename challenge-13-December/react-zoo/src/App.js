0600a09f181ebec63cd24299594b24f966baa225
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-12-13 13:06:26 +1100

    implemented species layout

--- challenge-13-December/react-zoo/src/App.js
+++ challenge-13-December/react-zoo/src/App.js
@@ -8,20 +8,84 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animalsInTheZoo: 0
+      animalsInTheZoo: 0,
+      totalMammals: 0,
+      totalReptiles: 0,
+      totalBirds: 0,
+      totalFish: 0
     }
+
   }
-  handleClick() {
+  increaseClick() {
     console.log('updating value')
     const currentTotal = this.state.animalsInTheZoo;
     this.setState({ animalsInTheZoo: currentTotal + 1 });
   }
+  decreaseClick() {
+    console.log('updating value')
+    const currentTotal = this.state.animalsInTheZoo;
+    this.setState({ animalsInTheZoo: currentTotal - 1 });
+  }
+  updateSpecies(specie, value){
+    console.log('specie',': ', specie);
+    console.log('increment',': ', value);
+    
+    if (specie=="Mammal"){
+      this.state.totalMammals +=value;
+      console.log("update")
+    }else if (specie=="Reptile"){
+      this.state.totalReptiles +=value;
+    }else if (specie=="Bird"){
+      this.state.totalBirds +=value;
+    }else if (specie=="Fish"){
+      this.state.totalFish += value;
+    }
+    
+    
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Number of Animals in the Zoo: {this.state.animalsInTheZoo}</h1> 
-        <Button label="Monkeys" updateClick={this.handleClick.bind(this)} />
-        <Button label="Tiger" updateClick={this.handleClick.bind(this)} />
+        <div class="container">
+          <Button label="Monkeys" 
+          specie="Mammal"
+          increaseClick={this.increaseClick.bind(this)}
+          decreaseClick={this.decreaseClick.bind(this)} 
+          updateSpecies={this.updateSpecies.bind(this)}
+          />
+
+          <Button label="Snake" 
+          specie="Reptile"
+          increaseClick={this.increaseClick.bind(this)}
+          decreaseClick={this.decreaseClick.bind(this)} 
+          updateSpecies={this.updateSpecies.bind(this)}
+          />
+
+          <Button label="Flamenco" 
+          specie="Bird"
+          increaseClick={this.increaseClick.bind(this)}
+          decreaseClick={this.decreaseClick.bind(this)} 
+          updateSpecies={this.updateSpecies.bind(this)}
+          />
+
+          <Button label="Shark" 
+          specie="Fish"
+          increaseClick={this.increaseClick.bind(this)}
+          decreaseClick={this.decreaseClick.bind(this)} 
+          updateSpecies={this.updateSpecies.bind(this)}
+          />
+        </div>
+
+        <div class="species">
+          <h1> Species </h1>
+          <h4> Total Mammals: {this.state.totalMammals} </h4>
+          <h4> Total Reptiles: {this.state.totalReptiles} </h4>
+          <h4> Total totalBirds: {this.state.totalBirds} </h4>
+          <h4> Total Fish: {this.state.totalFish} </h4>
+
+        </div>
 
       </div>
     );
--- challenge-13-December/react-zoo/src/Button.js
+++ challenge-13-December/react-zoo/src/Button.js
@@ -14,8 +14,21 @@ class Button extends Component {
   incrementClicks() {
     const currentNum = this.state.numOfClicks;
     this.setState({ numOfClicks: currentNum + 1});
-    this.props.updateClick()
+    this.props.increaseClick()
+    this.props.updateSpecies(this.props.specie, 1)
   }
+
+  decrementClicks() {
+    const currentNum = this.state.numOfClicks;
+    if (currentNum<=0){
+        return 
+    }
+    this.setState({ numOfClicks: currentNum - 1});
+    this.props.decreaseClick()
+    this.props.updateSpecies(this.props.specie, -1)
+
+  }
+
   render() {
     return (
         <div>
@@ -23,6 +36,9 @@ class Button extends Component {
             {this.props.label}
             </button>
             <h4>Number of {this.props.label}: {this.state.numOfClicks} </h4>
+            <button onClick={this.decrementClicks.bind(this)}>
+            Kill animal
+            </button>
         </div>
     )
     
