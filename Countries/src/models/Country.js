5aa92d5f0ae2336251219de33b4561162519821c
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-12-18 13:42:51 +1100

    added axios exercise and countriesByRegion.json

--- Countries/src/models/Country.js
+++ Countries/src/models/Country.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const constellationSchema = new mongoose.Schema({
   id: Number,
-  constellationName: String
+  name: String
 });
 
 module.exports = mongoose.model('Country', countrySchema);
