094cace029dce20013f2415728e7b7f714f045a5
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2018-12-30 11:09:02 +1100

    improved seed.js including an implementation of  mongoose.disconnect(). Also, started to reconstruct the project file structure implementing a MVC approach

---
+++ to-do-list-crud/backend/seed.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+//configuring mongoose 
+const databaseName = 'myList'
+
+mongoose.connect(`mongodb://localhost:27017/${databaseName}`);
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongod');
+});
+
+mongoose.connection.on('error', () => {
+  console.log('failed to connect to mongod');
+});
+
+
+const handleError = (error) => {
+    if (error){
+        console.log(error);
+    }
+}
+
+
+const Item = require('./models/Item');
+
+Item.deleteMany({}, err => {
+  if (err) {
+      console.log(err)
+  } else {
+      console.log("Item collection restarted.")
+  }
+})
+
+
+for (let index = 1; index <= 10; index++) {
+  Item.create({id:index, item: `Item # ${index}`},handleError);
+}
+
+// const userSchema = new mongoose.Schema({
+//   id: Number,
+//   username: String,
+//   password: String,
+// }, {collection: 'users'});
+
+const User = require('./models/User');
+User.deleteMany({}, err => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("User collection restarted.")
+    }
+})
+
+// const user1 = {
+//   id: 1,
+//   username: "user1",
+//   password: "password1"
+// }
+
+User.create({
+  id: 1,
+  username: "user1",
+  password: "password1"
+}, ()=> mongoose.disconnect());
+
+
--- to-do-list-crud/frontend/src/App.js
+++ to-do-list-crud/frontend/src/App.js
@@ -4,9 +4,11 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Items from './Items'
 import Item from './Item'
 
-import Users from './Users'
-import UsersSignUp from './UsersSignUp'
 
+
+import Users from './Users'
+import UsersNew from './UsersSignUp'
+import UsersShow from './Views/User/Show.js'
 
 
 class App extends Component {
@@ -15,13 +17,15 @@ class App extends Component {
       <div>
         <Router>
           <div>
+{/* 
             <Route exact path="/" component={Items} />
             <Route exact path="/items" component={Items} />
-            <Route exact path="/items/:id" component={Item} />
+            <Route exact path="/items/:id" component={Item} /> */}
 
 
             <Route exact path="/users" component={Users} />
-            <Route exact path="/users/sign-up" component={UsersSignUp} />
+            <Route exact path="/users/sign-up" component={UsersNew} />
+            <Route exact path="/users-show" component={UsersShow} />
 
 
           </div>
